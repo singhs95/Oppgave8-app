50bc92128cfecbb47580ce371010da1de6218ae3
singhs95 <gurdinho10@gmail.com>
2021-11-05 15:34:57 +0100

    Initial Commit

---
+++ Oppgave8 app/Oppgave8_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Oppgave8_appApp.swift
+//  Oppgave8 app
+//
+//  Created by Gursharnbir Singh on 05/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct Oppgave8_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
